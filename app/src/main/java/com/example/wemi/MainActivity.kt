65b0e580d3b7ee9d91ee55fd4e31e75faacf8caa
ksyi <ssh09015_@naver.com>
2022-08-24 00:47:09 +0900

    네이버 클라우드 연동

--- app/src/main/java/com/example/wemi/MainActivity.kt
+++ app/src/main/java/com/example/wemi/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.wemi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button2=findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            startActivity(Intent(this@MainActivity,MapView::class.java))
+        }
     }
 }
