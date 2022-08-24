cabebdb226c7d3159dc3730768b53296fe3c9418
Hajin-Lee0406 <glgkwls@naver.com>
2022-08-25 01:26:23 +0900

    지원

--- app/src/main/java/com/example/wemi/MainActivity.kt
+++ app/src/main/java/com/example/wemi/MainActivity.kt
@@ -6,13 +6,26 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var button2: Button
+    lateinit var supportBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         button2=findViewById<Button>(R.id.button2)
         button2.setOnClickListener {
             startActivity(Intent(this@MainActivity,MapView::class.java))
         }
+
+        supportBtn = findViewById<Button>(R.id.supportBtn)
+        supportBtn.setOnClickListener {
+            val intent = Intent(this, SupportActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
