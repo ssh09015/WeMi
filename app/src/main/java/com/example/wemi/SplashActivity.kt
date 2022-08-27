73e434490e5abb705fd52ba61304672eceed3af4
Hajin-Lee0406 <glgkwls@naver.com>
2022-08-28 00:44:18 +0900

    로그인 ã유지

--- app/src/main/java/com/example/wemi/SplashActivity.kt
+++ app/src/main/java/com/example/wemi/SplashActivity.kt
@@ -4,16 +4,38 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.example.wemi.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        },3000)    //3초 뒤에 스플래쉬 화면이 꺼진다
+        auth = Firebase.auth
+
+        //로그인 유지
+        if(auth.currentUser?.uid==null){
+            Log.d("SplashActivity", "null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            },3000)
+        }else{
+            Log.d("SplashActivity", "not null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },3000)
+        }
     }
 }
