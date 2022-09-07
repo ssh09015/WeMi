5d2e861135d44428a7dc4cff56654f92954ab332
unknown <glgkwls1@naver.com>
2022-09-07 20:53:44 +0900

    gkwls

--- app/src/main/java/com/example/wemi/SupportActivity.kt
+++ app/src/main/java/com/example/wemi/SupportActivity.kt
@@ -2,10 +2,21 @@ package com.example.wemi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wemi.support.SupportModel
 
 class SupportActivity : AppCompatActivity() {
+
+    private val supportDataList = mutableListOf<SupportModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+
+        val supportList = mutableListOf<SupportModel>()
+        supportList.add(SupportModel("a", "b", "c", "d"))
+
+
+
     }
 }
--- app/src/main/java/com/example/wemi/support/SupportModel.kt
+++ app/src/main/java/com/example/wemi/support/SupportModel.kt
@@ -1,8 +1,8 @@
 package com.example.wemi.support
 
 data class SupportModel (
-    val title : String?,
-    val content : String?,
-    val uid : String,
-    val time : String
+    val title : String = "",
+    val content : String = "",
+    val uid : String = "",
+    val time : String = ""
 )
