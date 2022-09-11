6bd06c520b7cf7f5e79fb62adba7ad484d5c3dec
Lee-Song-Ha <songha1744@swu.ac.kr>
2022-09-11 18:32:11 +0900

    BottomNavigation 수정

--- app/src/main/java/com/example/wemi/MainActivity.kt
+++ app/src/main/java/com/example/wemi/MainActivity.kt
@@ -9,43 +9,32 @@ import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button2: Button
-    lateinit var supportBtn: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navi_bar = findViewById<BottomNavigationView>(R.id.nav_bar)
-        button2=findViewById<Button>(R.id.button2)
-        button2.setOnClickListener {
-            startActivity(Intent(this,MapViewActivity::class.java))
-        }
+        val nav_bar = findViewById<BottomNavigationView>(R.id.nav_bar)
 
-        supportBtn = findViewById<Button>(R.id.supportBtn)
-        supportBtn.setOnClickListener {
-            val intent = Intent(this, SupportActivity::class.java)
-            startActivity(intent)
-        }
-
-        navi_bar.setOnItemSelectedListener { item ->
-
+        nav_bar.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.location -> {
                     val intent = Intent(this, MapViewActivity::class.java)
+                    intent.addFlags (Intent.FLAG_ACTIVITY_NO_ANIMATION)
                     startActivity(intent)
                     // Respond to navigation item 2 click
                     true
                 }
                 R.id.support -> {
                     val intent = Intent(this, SupportActivity::class.java)
+                    intent.addFlags (Intent.FLAG_ACTIVITY_NO_ANIMATION)
                     startActivity(intent)
                     // Respond to navigation item 3 click
                     true
                 }
                 R.id.myPage -> {
                     val intent = Intent(this, MypageActivity::class.java)
+                    intent.addFlags (Intent.FLAG_ACTIVITY_NO_ANIMATION)
                     startActivity(intent)
                     // Respond to navigation item 4 click
                     true
