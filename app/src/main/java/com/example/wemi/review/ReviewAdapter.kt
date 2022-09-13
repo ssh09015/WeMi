55b081cd50324e32e23ea1cb88d2ea4b4b2c414d
ksyi <ssh09015_@naver.com>
2022-09-13 22:37:38 +0900

    글씨 키움

--- app/src/main/java/com/example/wemi/review/ReviewAdapter.kt
+++ app/src/main/java/com/example/wemi/review/ReviewAdapter.kt
@@ -1,11 +1,8 @@
 package com.example.wemi.review
 
 import android.annotation.SuppressLint
-import android.app.Person
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
@@ -16,22 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.wemi.R
 import com.example.wemi.databinding.ActivityReviewDetailBinding
 
-
 class ReviewAdapter() :
     ListAdapter<ReviewModel, ReviewAdapter.ViewHolder>(diffUtill), Filterable {
-
-    val persons = ArrayList<ReviewModel>()
-
-
     var TAG="ReviewAdapter"
 
-    //원본 persons를 복제한 filteredPersons배열
-    //ilteredPersons배열을 사용자에게 보여줘야 하므로,
-    //이전에 어댑터가 persons를 참조한걸 싹 다 filteredPersons를 참조하도록 수정하셔야 합니다
-    var filteredPersons=ArrayList<ReviewModel>()
+    //var filteredPersons=ArrayList<ReviewModel>()
+    val persons =mutableListOf<ReviewModel>()
+    var filteredPersons=mutableListOf<ReviewModel>()
     var itemFilter=ItemFilter()
-
-
 
     inner class ViewHolder(private val binding: ActivityReviewDetailBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -41,7 +30,6 @@ class ReviewAdapter() :
         init{
             id=itemView.findViewById(R.id.id)
             comment=itemView.findViewById(R.id.comment)
-
         }
 
         fun bind(reviewModel: ReviewModel) {
@@ -49,7 +37,6 @@ class ReviewAdapter() :
             binding.comment.text = reviewModel.review
             binding.ratingbar.setIsIndicator(false)
             binding.ratingbar.rating = reviewModel.score.toFloat()
-
         }
     }
     init {
@@ -93,38 +80,32 @@ class ReviewAdapter() :
 
     //-- filter
     inner class ItemFilter : Filter() {
+        // SeachView에서 입력받은 문자열 charSequence에 따른 처리
         override fun performFiltering(charSequence: CharSequence): FilterResults {
             val filterString = charSequence.toString()
             val results = FilterResults()
             Log.d(TAG, "charSequence : $charSequence")
 
             //검색이 필요없을 경우를 위해 원본 배열을 복제
-            var filteredList: ArrayList<ReviewModel> = ArrayList<ReviewModel>()
+            var filteredList=mutableListOf<ReviewModel>()
             //공백제외 아무런 값이 없을 경우 -> 원본 배열
             if (filterString.trim { it <= ' ' }.isEmpty()) {
                 results.values = persons
                 results.count = persons.size
 
                 return results
-                //공백제외 2글자 이하인 경우 -> 이름으로만 검색
-            } /*else if (filterString.trim { it <= ' ' }.length <= 2) {
-                for (person in persons) {
-                    if (person.id.contains(filterString)) {
-                        filteredList.add(person)
-                    }
-                }
-                //그 외의 경우(공백제외 2글자 초과) -> 이름/전화번호로 검색
-            }*/ else {
+            }  else {
                 for (person in persons) {
                     if (person.id.contains(filterString) || person.review.contains(filterString)) {
-                       filteredList.add(person)
+                        /*ReviewModelList.add(reviewModel)
+                        reviewAdapter.submitList(ReviewModelList)*/
+                        filteredList.add(person)
 
                     }
                 }
             }
             results.values = filteredList
             results.count = filteredList.size
-
             return results
         }
 
@@ -135,9 +116,4 @@ class ReviewAdapter() :
             notifyDataSetChanged()
         }
     }
-
-
-
-
 }
-
